cec16c6706a617a981ea08c82c3e1dc6e4c782b9
jarlintonjara <alejarahi@gmail.com>
2022-08-29 08:52:51 -0500

    Integración con Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import '../css/app.css';
-
+import 'vuetify/styles' // Global CSS has to be imported
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
@@ -8,16 +11,21 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
-            .use(plugin)
+            .use(plugin).use(vuetify)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
 });
+
 
 InertiaProgress.init({ color: '#4B5563' });
